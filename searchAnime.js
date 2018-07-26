336e02687c82b1a13cbec6a82e0e4f017422850b
Fondson Tran <fondsont@gmail.com>
2018-07-26 19:23:11 -0400

    handle errors during initial search

--- searchAnime.js
+++ searchAnime.js
@@ -14,23 +14,29 @@ function scrapSearch(searchStr, res) {
         }
         // Check status code (200 is HTTP OK)
         if(response.statusCode === 200) {
-            let $ = cheerio.load(body);
-            
-            let url = $('#content')[0].children[9].children[0].children[1].children[3].children[1].attribs.href;
-
-            let pos = 0;
-            let slashCount = 0;
-            while (pos < url.length && slashCount < 4) {
-                if (url[pos++] == '/') {
-                    slashCount += 1;
+            try {
+                let $ = cheerio.load(body);
+                
+                let url = $('#content')[0].children[9].children[0].children[1].children[3].children[1].attribs.href;
+    
+                let pos = 0;
+                let slashCount = 0;
+                while (pos < url.length && slashCount < 4) {
+                    if (url[pos++] == '/') {
+                        slashCount += 1;
+                    }
                 }
+    
+                let id = ''
+                while (pos < url.length && url[pos] != '/') {
+                    id += url[pos++];
+                }
+                res.end( JSON.stringify({ error: false, id: id}) );
             }
-
-            let id = ''
-            while (pos < url.length && url[pos] != '/') {
-                id += url[pos++];
+            catch(e) {
+                res.end(JSON.stringify({ error: true, why: 'No such anime.'}));
+                return;
             }
-            res.end( JSON.stringify({ error: false, id: id}) );
         }
     });
 }
