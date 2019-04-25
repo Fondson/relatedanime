4906ab8bd7930b8aece4905001e5ce5dc0bc693a
Fondson Tran <fondsont@gmail.com>
2019-04-24 18:22:06 -0700

    https is not support, switch to http

--- pingSelf.js
+++ pingSelf.js
@@ -3,12 +3,12 @@ var neo4j = require('./neo4jHelper');
 
 // default every 5 minutes (300000)
 const DEFAUT = 300000;
-const LINK = 'https://relatedanime.herokuapp.com/';
+const LINK = 'http://relatedanime.herokuapp.com/';
 
 
 function pingHomepage(interval=DEFAUT) {
     setInterval(function() {
-        console.log('Pinging self at ' + LINK + '...')
+        console.log('Pinging self at ' + LINK + '...');
         http.get(LINK);
     }, interval); 
 }
