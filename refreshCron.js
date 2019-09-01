1b14926ccf0225c1c5045dcc2e92f14ab5733ce5
Fondson Tran <fondsont@gmail.com>
2019-09-01 17:47:30 -0400

    change refresh cron time to monday midnight; remove file logging (it is unsupported in heroku)

--- refreshCron.js
+++ refreshCron.js
@@ -3,13 +3,13 @@ var refreshRedis = require('./refreshRedis');
 var fs = require('fs');
 
 function start() {
-    const job = new CronJob('0 0 0 * * 6', async function() {
+    const job = new CronJob('0 0 0 * * 1', async function() {
         console.log('Starting refresh cron!');
-        const stream = fs.createWriteStream('refreshCron.txt', {flags:'a'});
-        stream.write('Starting at ' + new Date().toString() + '\n');
+        // const stream = fs.createWriteStream('refreshCron.txt', {flags:'a'});
+        // stream.write('Starting at ' + new Date().toString() + '\n');
         await refreshRedis.refresh();
-        stream.write('Finished at ' + new Date().toString() + '\n\n');
-        stream.end();
+        // stream.write('Finished at ' + new Date().toString() + '\n\n');
+        // stream.end();
         console.log(`Refresh cron complete!`);
     }, undefined, true, 'America/Los_Angeles');
 
