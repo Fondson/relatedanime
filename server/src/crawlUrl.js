9d73aa8dae125a9514bd074d8ad2edb395de0b93
Fondson Tran <fondsont@gmail.com>
2022-05-06 16:18:29 -0400

    Update proxy url

--- server/src/crawlUrl.js
+++ server/src/crawlUrl.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://myanimelist.net'
-const PROXY_URL = 'https://relatedanime-proxy.herokuapp.com/proxy'
+const PROXY_URL = 'https://relatedanime-proxy.herokuapp.com'
 
 function getUrl(proxy) {
   return proxy ? PROXY_URL : BASE_URL
