35f06d7b154c6bd2a94b5b4e13c022d792f848ed
Fondson Tran <fondsont@gmail.com>
2022-05-19 17:41:04 -0400

    Add title to entry components

--- next-frontend/src/components/Entry.tsx
+++ next-frontend/src/components/Entry.tsx
@@ -8,7 +8,7 @@ type EntryProps = {
 function Entry({ data }: EntryProps) {
   const { image, title, link, startDate } = data
   return (
-    <a className="block transition hover:scale-110" href={link}>
+    <a className="block transition hover:scale-110" href={link} title={title}>
       <EntryImage src={image} alt={title} />
       <div className="line-clamp-2 pt-2">{title}</div>
       <div className="italic text-gray-200">{startDate}</div>
--- next-frontend/src/components/SeasonalEntry.tsx
+++ next-frontend/src/components/SeasonalEntry.tsx
@@ -10,7 +10,7 @@ function SeasonalEntry({ data }: SeasonalEntryProps) {
   const { img, title, malType, id } = data
   return (
     <Link href={`/${malType}/${id}`}>
-      <a className="block w-28 transition hover:scale-110 md:w-44">
+      <a className="block w-28 transition hover:scale-110 md:w-44" title={title}>
         <EntryImage src={img} alt={title} />
         <div className="line-clamp-2 pt-2">{title}</div>
       </a>
